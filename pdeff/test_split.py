c106988e2967609ad96c9b64f9f624785af86048
Zahan Malkani <zahanm@gmail.com>
2012-01-08 21:15:08 +0000

    [smart split] test_split.py
    make it easier to figure out why it can't seem to find whitespace

---
+++ pdeff/test_split.py
@@ -0,0 +1,20 @@
+#! py_packages/bin/python
+
+import Image, ImageDraw
+
+from split import optimal_dividers
+
+def draw_lines(im):
+  optimal = optimal_dividers(im)
+  width, height = im.size
+  target = im.copy()
+  draw = ImageDraw.Draw(target)
+  for h in optimal:
+    draw.line((0, h, width, h), fill='red')
+  target.save('./tmp/lined.png')
+
+FNAME = './tmp/aaa.png'
+
+if __name__ == '__main__':
+  im = Image.open(FNAME)
+  draw_lines(im)
